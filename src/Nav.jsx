ec1e2901d8cd2a1bfc845bf9adc36b3f0b057043
moshe <moshehadar001@gmail.com>
2024-02-15 14:59:41 +0200

    fixed the log out

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import Posts from './component/Posts';
 import Todos from './component/Todos';
-import Albums from './component/Albums';
-import Photos from './component/Photos';
+// import Albums from './component/Albums';
+// import Photos from './component/Photos';
 import Logout from './component/Logout';
 import Info from './component/Info';
 import Login from './component/Login';
@@ -24,19 +24,19 @@ const Nav = ({ id, user, setUser, setIsLog }) => {
         <Link to='/Todos' className='navBarLinks'>
           Todos
         </Link>
-        <Link to='/' className='navBarLinks'>
+        <Link to='/Logout' className='navBarLinks'>
           Logout
         </Link>
       </nav>
 
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/Login' element={<Login />} />
         <Route path='/info' element={<Info />} />
-        <Route path='/Photos/:albumId' element={<Photos />} />
+        {/* <Route path='/Photos/:albumId' element={<Photos />} /> */}
         {/* <Route path='/Albums' element={<Albums id={id} user={user} />} /> */}
         <Route path='/Posts' element={<Posts id={id} user={user} />} />
         <Route path='/Todos' element={<Todos id={id} user={user} />} />
-        <Route index element={<Logout setUser={setUser} setIslog={setIsLog} />} />
+        <Route path='/Logout' element={<Logout setUser={setUser} setIslog={setIsLog} />} />
       </Routes>
     </div>
   );
